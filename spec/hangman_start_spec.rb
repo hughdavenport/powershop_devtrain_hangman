d8f464020f929ffb0728403d08b087b11c8c6d20
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-10 15:22:02 +1300

    Add tests to test default and non default starting scores

--- spec/hangman_start_spec.rb
+++ spec/hangman_start_spec.rb
@@ -2,10 +2,15 @@ require 'hangman'
 
 RSpec.describe Hangman, "#score" do
     context "with a new game" do
-        it "has a score of 7" do
-# FIXME: Remove the magic number somehow
+        it "has a default score of 7" do
+            default_score = 7
             game = Hangman.new
-            expect(game.score).to eq 7
+            expect(game.score).to eq default_score
+        end
+        it "can have a score of 6" do
+            score = 6
+            game = Hangman.new(score)
+            expect(game.score).to eq score
         end
     end
 end
