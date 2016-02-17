0a304c287534fdd2d32c64abf696865de5d682b2
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-17 14:01:13 +1300

    Add french and spanish translations

---
+++ lib/langs/es.rb
@@ -0,0 +1,11 @@
+STRINGS = {
+    :pleaseenteraletter         => "Por favor, introduzca una letra: ",
+    :inputisnotlowercase        => "La entrada no es minúscula",
+    :inputhasalreadybeenguessed => "De entrada ya se ha conjecturado",
+    :inputisinvalid             => "Entrada no es valida",
+    :gameover                   => "¡JUEGA TERMINADO!",
+    :youwon                     => "¡Ganaste!",
+    :youlost                    => "¡Perdiste!",
+    :youhavelivesleft           => "Ha :lives vidas izquierda",
+    :currentguessis             => "Conjectura actual es: :guess",
+}
---
+++ lib/langs/fr.rb
@@ -0,0 +1,11 @@
+STRINGS = {
+    :pleaseenteraletter         => "S'il vous plaît entrer une lettre: ",
+    :inputisnotlowercase        => "Entrée ne sont pas minuscules",
+    :inputhasalreadybeenguessed => "Entrée a déjà été deviné",
+    :inputisinvalid             => "Entrée est invalide",
+    :gameover                   => "JEU TERMINÉ!",
+    :youwon                     => "Vous avez gagné!",
+    :youlost                    => "Vous avez perdu!",
+    :youhavelivesleft           => "Vous avez :lives vies gauche",
+    :currentguessis             => "Conjecture actualle est: :guess",
+}
