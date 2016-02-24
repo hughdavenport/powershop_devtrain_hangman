388746a742b625d96a2774b989d314cd63cabe16
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-25 12:09:06 +1300

    Clean up wordlist spec file

--- spec/wordlist_spec.rb
+++ spec/wordlist_spec.rb
@@ -3,53 +3,59 @@ require 'errors'
 
 RSpec.describe Wordlist do
   context "default wordlist" do
-    before do
-      @wordlist = Wordlist.new
-    end
     it "should get return a word" do
-      word = @wordlist.get_word
+      word = subject.get_word
       expect(word).not_to be_nil
       expect(word.length).to be > 0
     end
     it "should not return same word twice" do
-      word1 = @wordlist.get_word
-      word2 = @wordlist.get_word
+      word1 = subject.get_word
+      word2 = subject.get_word
       expect(word1).not_to eq word2
     end
     it "should be single line, all lower case, with no spaces" do
-      word = @wordlist.get_word
+      word = subject.get_word
       expect(word).to match /\A[a-z]*\z$/
     end
     it "should be between 4 and 15 characters" do
-      word = @wordlist.get_word
+      word = subject.get_word
       expect(word.length).to be >= 4
       expect(word.length).to be <= 15
     end
   end
-  context "empty wordlist" do
+  context "empty wordlist file" do
+    subject { Wordlist.new("/dev/null") }
+
     it "should fail on empty wordlist" do
-      expect { Wordlist.new("/dev/null") }.to raise_error(NoUsableWordsError)
+      expect { subject }.to raise_error(NoUsableWordsError)
     end
   end
-  context "invalid wordlist" do
+  context "invalid wordlist path" do
+    subject { Wordlist.new("/nonexistantpath") }
+
     it "should fail on an invalid path" do
-      expect { Wordlist.new("/nonexistantpath") }.to raise_error(NoUsableWordsError)
-    end
-    it "should fail on an invalid wordlist" do
-      expect { Wordlist.new("tests/invalid") }.to raise_error(NoUsableWordsError)
+      expect { subject }.to raise_error(NoUsableWordsError)
     end
   end
-  context "valid wordlist" do
-    before do
-      @wordlist = Wordlist.new("tests/valid")
-      @words = [
+  context "invalid wordlist file" do
+    subject { Wordlist.new("tests/invalid") }
+
+    it "should fail on an invalid wordlist" do
+      expect { subject }.to raise_error(NoUsableWordsError)
+    end
+  end
+  context "valid wordlist file" do
+    let(:words) {
+      [
         'abcd',
         'abcdefghijklmno',
       ]
-    end
+    }
+    subject { Wordlist.new("tests/valid") }
+
     it "should return a word from the wordlist" do
-      word = @wordlist.get_word
-      expect(@words).to include word
+      word = subject.get_word
+      expect(words).to include word
     end
   end
 end
