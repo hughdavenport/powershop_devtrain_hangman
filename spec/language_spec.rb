3457fa5d7ba0791b0f386d9950740eb57938b55c
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-02-25 13:23:22 +1300

    Add subjects to lang spec

--- spec/language_spec.rb
+++ spec/language_spec.rb
@@ -3,143 +3,138 @@ require 'errors'
 
 RSpec.describe Language do
   context "default" do
-    before do
-      @language = LanguageLoader.load
-    end
+    subject { LanguageLoader.load }
     it "should default to english" do
-      expect(@language.lang).to eq "en"
+      expect(subject.lang).to eq "en"
     end
   end
   context "English" do
-    before do
-      @language = LanguageLoader.load "en"
-    end
+    subject { LanguageLoader.load("en") }
+
     it "should return the correct phrase for entering a letter" do
-      expect(@language.get_string(:pleaseenteraletter)).to eq "Please enter a letter: "
+      expect(subject.get_string(:pleaseenteraletter)).to eq "Please enter a letter: "
     end
     it "should return the correct phrase for non lower case letters" do
-      expect(@language.get_string(:inputisnotlowercase)).to eq "Input is not lower case"
+      expect(subject.get_string(:inputisnotlowercase)).to eq "Input is not lower case"
     end
     it "should return the correct phrase for already guessed letters" do
-      expect(@language.get_string(:inputhasalreadybeenguessed)).to eq "Input has already been guessed"
+      expect(subject.get_string(:inputhasalreadybeenguessed)).to eq "Input has already been guessed"
     end
     it "should return the correct phrase for invalid letters" do
-      expect(@language.get_string(:inputisinvalid)).to eq "Input is invalid"
+      expect(subject.get_string(:inputisinvalid)).to eq "Input is invalid"
     end
     it "should return the correct phrase for game over" do
-      expect(@language.get_string(:gameover)).to eq "GAME OVER!"
+      expect(subject.get_string(:gameover)).to eq "GAME OVER!"
     end
     it "should return the correct phrase for winning" do
-      expect(@language.get_string(:youwon)).to eq "You won!"
+      expect(subject.get_string(:youwon)).to eq "You won!"
     end
     it "should return the correct phrase for losing" do
-      expect(@language.get_string(:youlost)).to eq "You lost!"
+      expect(subject.get_string(:youlost)).to eq "You lost!"
     end
     it "should return the correct phrase for lives remaining in current game, with 4 lives left" do
-      expect(@language.get_string(:youhavelivesremaining, {:lives => 4})).to eq "You have 4 lives remaining"
+      expect(subject.get_string(:youhavelivesremaining, {:lives => 4})).to eq "You have 4 lives remaining"
     end
     it "should return the correct phrase for lives remaining in finished game, with 0 lives left" do
-      expect(@language.get_string(:youhadlivesremaining, {:lives => 0})).to eq "You had 0 lives remaining"
+      expect(subject.get_string(:youhadlivesremaining, {:lives => 0})).to eq "You had 0 lives remaining"
     end
     it "should return the correct phrase for the current guess, with a guess of h_ngm_n" do
-      expect(@language.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Current guess is: h_ngm_n"
+      expect(subject.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Current guess is: h_ngm_n"
     end
     it "should return the correct phrase for final guess, with a guess of hangman" do
-      expect(@language.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Final guess was: hangman"
+      expect(subject.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Final guess was: hangman"
     end
     it "should return the correct phrase for current guess letters, with a e i o u" do
-      expect(@language.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "You have guessed: a e i o u"
+      expect(subject.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "You have guessed: a e i o u"
     end
     it "should return the correct phrase for final guessed letters, with a b c z x y" do
-      expect(@language.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "You had guessed: a b c z x y"
+      expect(subject.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "You had guessed: a b c z x y"
     end
   end
   context "Spanish" do
-    before do
-      @language = LanguageLoader.load "es"
-    end
+    subject { LanguageLoader.load("es") }
+
     it "should return the correct phrase for entering a letter" do
-      expect(@language.get_string(:pleaseenteraletter)).to eq "Por favor, introduzca una letra: "
+      expect(subject.get_string(:pleaseenteraletter)).to eq "Por favor, introduzca una letra: "
     end
     it "should return the correct phrase for non lower case letters" do
-      expect(@language.get_string(:inputisnotlowercase)).to eq "La entrada no es minúscula"
+      expect(subject.get_string(:inputisnotlowercase)).to eq "La entrada no es minúscula"
     end
     it "should return the correct phrase for already guessed letters" do
-      expect(@language.get_string(:inputhasalreadybeenguessed)).to eq "De entrada ya se ha conjecturado"
+      expect(subject.get_string(:inputhasalreadybeenguessed)).to eq "De entrada ya se ha conjecturado"
     end
     it "should return the correct phrase for invalid letters" do
-      expect(@language.get_string(:inputisinvalid)).to eq "Entrada no es valida"
+      expect(subject.get_string(:inputisinvalid)).to eq "Entrada no es valida"
     end
     it "should return the correct phrase for game over" do
-      expect(@language.get_string(:gameover)).to eq "¡JUEGA TERMINADO!"
+      expect(subject.get_string(:gameover)).to eq "¡JUEGA TERMINADO!"
     end
     it "should return the correct phrase for winning" do
-      expect(@language.get_string(:youwon)).to eq "¡Ganaste!"
+      expect(subject.get_string(:youwon)).to eq "¡Ganaste!"
     end
     it "should return the correct phrase for losing" do
-      expect(@language.get_string(:youlost)).to eq "¡Perdiste!"
+      expect(subject.get_string(:youlost)).to eq "¡Perdiste!"
     end
     it "should return the correct phrase for lives remaining in current game, with 4 lives left" do
-      expect(@language.get_string(:youhavelivesremaining, {:lives => 4})).to eq "Usted tiene 4 vidas restante"
+      expect(subject.get_string(:youhavelivesremaining, {:lives => 4})).to eq "Usted tiene 4 vidas restante"
     end
     it "should return the correct phrase for lives remaining in finished game, with 0 lives left" do
-      expect(@language.get_string(:youhadlivesremaining, {:lives => 0})).to eq "Usted tenía 0 vidas restante"
+      expect(subject.get_string(:youhadlivesremaining, {:lives => 0})).to eq "Usted tenía 0 vidas restante"
     end
     it "should return the correct phrase for the current guess, with a guess of h_ngm_n" do
-      expect(@language.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Conjectura actual es: h_ngm_n"
+      expect(subject.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Conjectura actual es: h_ngm_n"
     end
     it "should return the correct phrase for final guess, with a guess of hangman" do
-      expect(@language.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Conjectura final fue: hangman"
+      expect(subject.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Conjectura final fue: hangman"
     end
     it "should return the correct phrase for current guess letters, with a e i o u" do
-      expect(@language.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "Usted tiene adivinado: a e i o u"
+      expect(subject.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "Usted tiene adivinado: a e i o u"
     end
     it "should return the correct phrase for final guessed letters, with a b c z x y" do
-      expect(@language.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "Usted ha adivinado: a b c z x y"
+      expect(subject.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "Usted ha adivinado: a b c z x y"
     end
   end
   context "French" do
-    before do
-      @language = LanguageLoader.load "fr"
-    end
+    subject { LanguageLoader.load("fr") }
+
     it "should return the correct phrase for entering a letter" do
-      expect(@language.get_string(:pleaseenteraletter)).to eq "S'il vous plaît entrer une lettre: "
+      expect(subject.get_string(:pleaseenteraletter)).to eq "S'il vous plaît entrer une lettre: "
     end
     it "should return the correct phrase for non lower case letters" do
-      expect(@language.get_string(:inputisnotlowercase)).to eq "Entrée ne sont pas minuscules"
+      expect(subject.get_string(:inputisnotlowercase)).to eq "Entrée ne sont pas minuscules"
     end
     it "should return the correct phrase for already guessed letters" do
-      expect(@language.get_string(:inputhasalreadybeenguessed)).to eq "Entrée a déjà été deviné"
+      expect(subject.get_string(:inputhasalreadybeenguessed)).to eq "Entrée a déjà été deviné"
     end
     it "should return the correct phrase for invalid letters" do
-      expect(@language.get_string(:inputisinvalid)).to eq "Entrée est invalide"
+      expect(subject.get_string(:inputisinvalid)).to eq "Entrée est invalide"
     end
     it "should return the correct phrase for game over" do
-      expect(@language.get_string(:gameover)).to eq "JEU TERMINÉ!"
+      expect(subject.get_string(:gameover)).to eq "JEU TERMINÉ!"
     end
     it "should return the correct phrase for winning" do
-      expect(@language.get_string(:youwon)).to eq "Vous avez gagné!"
+      expect(subject.get_string(:youwon)).to eq "Vous avez gagné!"
     end
     it "should return the correct phrase for losing" do
-      expect(@language.get_string(:youlost)).to eq "Vous avez perdu!"
+      expect(subject.get_string(:youlost)).to eq "Vous avez perdu!"
     end
     it "should return the correct phrase for lives remaining in current game, with 4 lives left" do
-      expect(@language.get_string(:youhavelivesremaining, {:lives => 4})).to eq "Vous avez 4 vies restant"
+      expect(subject.get_string(:youhavelivesremaining, {:lives => 4})).to eq "Vous avez 4 vies restant"
     end
     it "should return the correct phrase for lives remaining in finished game, with 0 lives left" do
-      expect(@language.get_string(:youhadlivesremaining, {:lives => 0})).to eq "Vous aviez 0 vies restant"
+      expect(subject.get_string(:youhadlivesremaining, {:lives => 0})).to eq "Vous aviez 0 vies restant"
     end
     it "should return the correct phrase for the current guess, with a guess of h_ngm_n" do
-      expect(@language.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Conjecture actualle est: h_ngm_n"
+      expect(subject.get_string(:currentguessis, {:guess => "h_ngm_n"})).to eq "Conjecture actualle est: h_ngm_n"
     end
     it "should return the correct phrase for final guess, with a guess of hangman" do
-      expect(@language.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Conjecture finale était: hangman"
+      expect(subject.get_string(:finalguesswas, {:guess => "hangman"})).to eq "Conjecture finale était: hangman"
     end
     it "should return the correct phrase for current guess letters, with a e i o u" do
-      expect(@language.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "Vous avez deviné: a e i o u"
+      expect(subject.get_string(:youhaveguessed, {:guesses => "a e i o u"})).to eq "Vous avez deviné: a e i o u"
     end
     it "should return the correct phrase for final guessed letters, with a b c z x y" do
-      expect(@language.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "Vous aviez deviné: a b c z x y"
+      expect(subject.get_string(:youhadguessed, {:guesses => "a b c z x y"})).to eq "Vous aviez deviné: a b c z x y"
     end
   end
 end
