4e5070b92b7b2dcdb43f8b7d469cdc19dc250613
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-01 09:52:41 +1300

    Test game controller get_error method

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -25,7 +25,20 @@ RSpec.describe Game do
 
     allow(hangman).to receive(:finished?) { false }
     allow(hangman).to receive(:validate_letter) do |letter|
-
+      # just some testing data
+      if letter == 'a'
+        # good, always (not in real hangman object, but thats tested elsewhere
+        nil
+      elsif letter == 'A'
+        # Obviously capital
+        raise NotLowerCaseLetterError
+      elsif letter == 'z'
+        # Pretend we've already tested this before
+        raise AlreadyGuessedError
+      elsif letter == '~' || letter == '1'
+        # Obviously not a letter
+        raise ValidateError
+      end
     end
 
     hangman
@@ -33,7 +46,24 @@ RSpec.describe Game do
 
   subject { Game.new(hangman: hangman, presenter: presenter) }
 
-# get_error
+  describe "#get_error" do
+    it "should fail on a capital letter" do
+      expect(subject.get_error('A')).to eq :inputisnotlowercase
+    end
+    it "should fail on an already guessed letter" do
+      expect(subject.get_error('z')).to eq :inputhasalreadybeenguessed
+    end
+    it "should fail on a tilde as invalid" do
+      expect(subject.get_error('~')).to eq :inputisinvalid
+    end
+    it "should fail on a digit as invalid" do
+      expect(subject.get_error('1')).to eq :inputisinvalid
+    end
+    it "should accept a 'valid' letter" do
+      expect(subject.get_error('a')).to be_nil
+    end
+  end
+
 # get_guess
 # run
 
