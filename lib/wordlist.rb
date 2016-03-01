70447c122e017195b941876521dd3f55c796c245
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-02 12:07:36 +1300

    Shorten the wordlist method more

--- lib/wordlist.rb
+++ lib/wordlist.rb
@@ -2,9 +2,7 @@ class Wordlist
   attr_reader :words
 
   def initialize(filename="/usr/share/dict/words")
-    @words = File.open(filename, "r") do |f|
-      f.each_line.map { |line| line.chomp }.select { |line| line =~ /^[a-z]{4,15}$/ }
-    end
+    @words = File.readlines(filename).map { |line| line.chomp }.select { |line| line =~ /^[a-z]{4,15}$/ }
     raise NoUsableWordsError if @words.nil? || @words.empty?
   end
 
