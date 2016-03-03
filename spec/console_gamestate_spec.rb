02fb75f41c21424e489ba382b82a6cbc2778390b
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-03 14:01:52 +1300

    Reword tests in console_gamestate
    Now follows a much clearer path with describes and contexts

--- spec/console_gamestate_spec.rb
+++ spec/console_gamestate_spec.rb
@@ -1,4 +1,5 @@
 require 'console_gamestate'
+require 'language'
 
 RSpec.describe ConsoleGamestate do
   let(:language) { Language.new } # Will always just do [[:langstringhere||:argshere]] syntax, test langs seperately
@@ -16,84 +17,88 @@ RSpec.describe ConsoleGamestate do
     hangman
   end
 
-  describe "#state" do
-    context "Not finished" do
-      before          { allow(hangman).to receive(:finished?) { false } }
-      let(:gamestate) { subject.state(hangman, language) }
+  subject { ConsoleGamestate.new.state(hangman, language) }
 
-      it "should not have game over" do
-        expect(gamestate).not_to include("[[game_over]]")
+  context "when the game is not finished" do
+    before { allow(hangman).to receive(:finished?) { false } }
+
+    describe "#state" do
+      it "should not include game over" do
+        expect(subject).not_to include("[[game_over]]")
       end
 
-      it "should not have you won" do
-        expect(gamestate).not_to include("[[you_won]]")
+      it "should not include you won" do
+        expect(subject).not_to include("[[you_won]]")
       end
 
-      it "should not have you lost" do
-        expect(gamestate).not_to include("[[you_lost]]")
+      it "should not include you lost" do
+        expect(subject).not_to include("[[you_lost]]")
       end
 
-      it "should have lives remaining" do
-        expect(gamestate).to include("[[you_have_lives_remaining||{:lives=>#{lives}}]]")
+      it "should include the lives remaining" do
+        expect(subject).to include("[[you_have_lives_remaining||{:lives=>#{lives}}]]")
       end
 
-      it "should have current guess" do
-        expect(gamestate).to include("[[current_guess_is||{:guess=>\"#{guess_word}\"}]]")
+      it "should include the current guess" do
+        expect(subject).to include("[[current_guess_is||{:guess=>\"#{guess_word}\"}]]")
       end
 
-      it "should have letters guessed" do
-        expect(gamestate).to include("[[you_have_guessed||{:guesses=>\"#{guesses.join(" ")}\"}]]")
+      it "should include the letters guessed" do
+        expect(subject).to include("[[you_have_guessed||{:guesses=>\"#{guesses.join(" ")}\"}]]")
+      end
+    end
+  end
+
+  context "when the game is finished" do
+    before { allow(hangman).to receive(:finished?) { true } }
+    before { allow(hangman).to receive(:won?) { true } } # Not tested, required for gamestate call to happen
+
+    describe "#state" do
+      it "should include game over" do
+        expect(subject).to include("[[game_over]]")
+      end
+
+      it "should include the final lives remaining" do
+        expect(subject).to include("[[you_had_lives_remaining||{:lives=>#{lives}}]]")
+      end
+
+      it "should include the final guess" do
+        expect(subject).to include("[[final_guess_was||{:guess=>\"#{guess_word}\"}]]")
+      end
+
+      it "should include the letters guessed" do
+        expect(subject).to include("[[you_had_guessed||{:guesses=>\"#{guesses.join(" ")}\"}]]")
+      end
+
+      it "should include the game word" do
+        expect(subject).to include("[[the_word_was||{:word=>\"#{word}\"}]]")            # Don't include the string args, tested elsewhere
       end
     end
 
-    context "Finished" do
-      before          { allow(hangman).to receive(:finished?) { true } }
-      before          { allow(hangman).to receive(:won?) { true } } # Not tested, required for gamestate call to happen
-      let(:gamestate) { subject.state(hangman, language) }
+    context "and the game is won" do
+      before { allow(hangman).to receive(:won?) { true } }
 
-      it "should have game over" do
-        expect(gamestate).to include("[[game_over]]")
-      end
-
-      it "should have final lives remaining" do
-        expect(gamestate).to include("[[you_had_lives_remaining||{:lives=>#{lives}}]]")
-      end
-
-      it "should have final guess" do
-        expect(gamestate).to include("[[final_guess_was||{:guess=>\"#{guess_word}\"}]]")
-      end
-
-      it "should have letters guessed" do
-        expect(gamestate).to include("[[you_had_guessed||{:guesses=>\"#{guesses.join(" ")}\"}]]")
-      end
-
-      it "should have actual word" do
-        expect(gamestate).to include("[[the_word_was||{:word=>\"#{word}\"}]]")            # Don't include the string args, tested elsewhere
-      end
-
-      context "Won" do
-        before          { allow(hangman).to receive(:won?) { true } }
-        let(:gamestate) { subject.state(hangman, language) }
-
-        it "should have you won" do
-          expect(gamestate).to include("[[you_won]]")
+      describe "#state" do
+        it "should include you won" do
+          expect(subject).to include("[[you_won]]")
         end
 
-        it "should not have you lost" do
-          expect(gamestate).not_to include("[[you_lost]]")
+        it "should not include you lost" do
+          expect(subject).not_to include("[[you_lost]]")
         end
       end
+    end
 
-      context "Lost" do
-        before          { allow(hangman).to receive(:won?) { false } }
-        let(:gamestate) { subject.state(hangman, language) }
+    context "and the game is lost" do
+      before { allow(hangman).to receive(:won?) { false } }
 
-        it "should not have you won" do
-          expect(gamestate).not_to include("[[you_won]]")
+      describe "#state" do
+        it "should not include you won" do
+          expect(subject).not_to include("[[you_won]]")
         end
 
-        it "should have you lost" do
-          expect(gamestate).to include("[[you_lost]]")
+        it "should include you lost" do
+          expect(subject).to include("[[you_lost]]")
         end
       end
     end
