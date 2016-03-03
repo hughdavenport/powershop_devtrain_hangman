76a8e3af0b3c1660ad96356f716e80a8485291d4
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-03 16:05:17 +1300

    Reword specs for wordlist

--- spec/wordlist_spec.rb
+++ spec/wordlist_spec.rb
@@ -2,67 +2,71 @@ require 'wordlist'
 require 'errors'
 
 RSpec.describe Wordlist do
-  context "default wordlist" do
-    it "should get return a word" do
-      word = subject.word
-      expect(word).not_to be_nil
-      expect(word.length).to be > 0
-    end
+  context "with the default initialization" do
+    describe "#word" do
+      it "should return a non empty word" do
+        word = subject.word
+        expect(word).not_to be_nil
+        expect(word.length).to be > 0
+      end
 
-    it "should not return same word twice" do
-      word1 = subject.word
-      word2 = subject.word
-      expect(word1).not_to eq word2
-    end
+      it "should return a different word with a second call" do
+        word1 = subject.word
+        word2 = subject.word
+        expect(word1).not_to eq word2
+      end
 
-    it "should be single line, all lower case, with no spaces" do
-      word = subject.word
-      expect(word).to match /\A[a-z]*\z$/
-    end
+      it "should return a word that only contains lower case characters" do
+        word = subject.word
+        expect(word).to match /\A[a-z]*\z$/
+      end
 
-    it "should be between 4 and 15 characters" do
-      word = subject.word
-      expect(word.length).to be >= 4
-      expect(word.length).to be <= 15
-    end
-  end
-
-  context "empty wordlist file" do
-    subject { Wordlist.new("/dev/null") }
-
-    it "should fail on empty wordlist" do
-      expect { subject }.to raise_error(NoUsableWordsError)
+      it "should return a word that is between 4 and 15 characters long" do
+        word = subject.word
+        expect(word.length).to be >= 4
+        expect(word.length).to be <= 15
+      end
     end
   end
 
-  context "invalid wordlist path" do
-    subject { Wordlist.new("/nonexistantpath") }
+  describe "#initialize" do
+    context "with an argument of an empty wordlist" do
+      subject { Wordlist.new("tests/empty") }
 
-    it "should fail on an invalid path" do
-      expect { subject }.to raise_error(Exception)
+      it "should raise an error" do
+        expect { subject }.to raise_error(NoUsableWordsError)
+      end
     end
-  end
 
-  context "invalid wordlist file" do
-    subject { Wordlist.new("tests/invalid") }
+    context "with an argument of an non-existant file" do
+      subject { Wordlist.new("tests/nonexistant") }
 
-    it "should fail on an invalid wordlist" do
-      expect { subject }.to raise_error(NoUsableWordsError)
+      it "should raise an exception" do
+        expect { subject }.to raise_error(Exception)
+      end
     end
-  end
 
-  context "valid wordlist file" do
-    let(:words) {
-      [
-        'abcd',
-        'abcdefghijklmno',
-      ]
-    }
-    subject { Wordlist.new("tests/valid") }
+    context "with an argument of a wordlist that only contains invalid words" do
+      subject { Wordlist.new("tests/invalid") }
 
-    it "should return a word from the wordlist" do
-      word = subject.word
-      expect(words).to include word
+      it "should raise an error" do
+        expect { subject }.to raise_error(NoUsableWordsError)
+      end
+    end
+
+    context "with an argument of a small known wordlist" do
+      let(:words) {
+        [
+          'abcd',
+          'abcdefghijklmno',
+        ]
+      }
+      subject { Wordlist.new("tests/valid") }
+
+      it "should return a word from that wordlist" do
+        word = subject.word
+        expect(words).to include word
+      end
     end
   end
 end
