167c47c6b25fcfaafce140a5cb12c42d73858c52
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-03-03 14:57:27 +1300

    Clean up console presenter spec

--- spec/console_presenter_spec.rb
+++ spec/console_presenter_spec.rb
@@ -21,6 +21,7 @@ RSpec.describe ConsolePresenter do
     let(:io)       { ConsoleIO.new(input: input, output: output) }
     let(:language) { Language.new } # Will always just do [[:langstringhere||:argshere]] syntax, test langs seperately
 
+    let(:ask_for_letter_message) { :please_enter_a_letter}
     let(:error) { :testingerror }
 
     let(:word) { "pneumatic" }
@@ -40,53 +41,65 @@ RSpec.describe ConsolePresenter do
       gamestate
     end
 
+    let(:ansi_beginning_of_line) { "\e[H" }
+    let(:ansi_erase_screen)      { "\e[2J" }
+
     subject { ConsolePresenter.new(io: io, language: language, gamestate: gamestate) }
 
-    describe "Error handling" do
-      it "should not display anything when no error" do
-        subject.display_error
-        expect(output.printed).to be_empty
-        expect(output.flushed).to be_empty
+    describe "#display_error" do
+      context "with no arguments" do
+        it "should not display anything" do
+          subject.display_error
+          expect(output.printed).to be_empty
+          expect(output.flushed).to be_empty
+        end
       end
 
-      it "should display the error we add with a new line" do
-        subject.display_error(error)
-        expect(output.printed).to be_empty
-        expect(output.flushed).to eq "[["+error.to_s+"]]\n"
+      context "with a test error" do
+        it "should display the error" do
+          subject.display_error(error)
+          expect(output.printed).to be_empty
+          expect(output.flushed).to eq "[[#{error}]]\n"
+        end
       end
     end
 
-    describe "Displaying game" do
+    describe "#display_game" do
       context "with no errors" do
-        it "should have cleared screen, then print the gamestate" do
+        it "should clear the screen, then display the gamestate" do
           subject.display_game(hangman)
           expect(output.printed).to be_empty
-          expect(output.flushed).to eq "\e[H\e[2J" + gamestate.state(hangman, language) + "\n"
+          expect(output.flushed).to eq "#{ansi_beginning_of_line}#{ansi_erase_screen}" + gamestate.state(hangman, language) + "\n"
         end
       end
 
       context "with an error" do
-        it "should have cleared screen, then print the error, then print the gamestate" do
+        it "should clear the screen, then display the error, then display the gamestate" do
           subject.display_game(hangman, error)
           expect(output.printed).to be_empty
-          expect(output.flushed).to eq "\e[H\e[2J" + "[["+error.to_s+"]]\n" + gamestate.state(hangman, language) + "\n"
+          expect(output.flushed).to eq "#{ansi_beginning_of_line}#{ansi_erase_screen}" + "[[#{error}]]\n" + gamestate.state(hangman, language) + "\n"
         end
       end
     end
 
-    describe "Asking for letters" do
-      let(:letter)  { "a" }
-      let(:message) { :please_enter_a_letter}
-      before        { allow(input).to receive(:getch) { letter } }
-
-      it "Should print a message asking for a letter" do
-        subject.ask_for_letter
-        expect(output.printed).to be_empty
-        expect(output.flushed).to eq "[["+message.to_s+"]]"
+    describe "#ask_for_letter" do
+      context "with no arguments" do
+        it "Should print a message asking for a letter" do
+          subject.ask_for_letter
+          expect(output.printed).to be_empty
+          expect(output.flushed).to eq "[[#{ask_for_letter_message}]]"
+        end
       end
+    end
 
-      it "Should return the letter we give" do
-        expect(subject.getch).to eq letter
+    describe "#getch" do
+      context "when input is a single character" do
+        let(:character)  { "a" }
+        before           { allow(input).to receive(:getch) { character } }
+
+        it "should return the same character" do
+          expect(subject.getch).to eq character
+        end
       end
     end
   end
